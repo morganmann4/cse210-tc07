0e0610a47495d8620ff665ea0b8650ca0100bd39
Morgan Mann <momomann4@gmail.com>
2021-06-03 16:23:49 -0600

    Started code for output and active actors class

--- Speed/speed/game/active_actors.py
+++ Speed/speed/game/active_actors.py
@@ -1,3 +1,6 @@
+from game.actor import Actor
+from game.score import Score
+
 class Active_Actors:
     """ Morgan, this class is the 'correcting' class that we had talked about as a group. after getting into the psuedo code a bit
     I thought this might be a bit more accurate of a name for this class since we are going to keep the list of active actors here
@@ -17,3 +20,51 @@ class Active_Actors:
         new_word(Actor): takes in a new Actor object that is the new word being added to the screen and puts it in the _active_words list.
         attempt(string): takes in string from the director class and checks it against the _active_words list to see if it is a current active word (returns True for correct and False for incorrect). If correct it also needs to use the kill_word function in Actor and remove it from the active word list
     """
+
+    def __init__(self):
+        self._active_words = []
+        self._actor = Actor()
+        self._score = Score()
+
+
+
+    def check_guess(self, guess):
+        """ Takes the guess from director and checks the list of words. If the guess is correct it sends the word to score, 
+            then sends the word to be killed and calls the replace_word method
+
+        Args: 
+            self (ActiveActors): An instance of Active Actors
+            guess (string): The players guess
+        """
+
+        for i in range (0, len(_active_words)):
+            if guess == self._active_words[i]:
+                self._actor.score(guess)
+                self._actor.kill_word(guess) 
+                self.replace_word(i)
+                return True
+
+            else:
+                return False 
+
+    def replace_word(self, position):
+        """Replaces the guessed word with a new word
+
+        Args:
+            self (ActiveActors): An instance of Active Actors
+            position (integer): the guessed words postition in string
+        """
+
+        new_word = self.actor._word #gets new word from actor class
+        self._active_words.insert(position, new_word) #puts new word in olds words list position
+
+    def create_list(self):
+        """Creates the list of active words"""
+        pass
+
+    def return_list(self):
+        """Gets the list of active words"""
+        return self._active_words #returns updated word list
+
+
+
--- Speed/speed/game/output.py
+++ Speed/speed/game/output.py
@@ -1,6 +1,7 @@
 import sys
 from game import constants
 from asciimatics.widgets import Frame
+from game.active_actors import Active_Actors
 
 class Screen:
     """Outputs the game state. The responsibility of the class of objects is to draw the game state on the terminal. 
@@ -15,3 +16,23 @@ class Screen:
         needs to have a function to draw words on screen in given places
         ??? Lets talk, we need to have a function to put the buffer at the bottom of the screen and the score at the top
     """
+
+    def __init__(self, screen):
+        self._screen = screen
+        #self._active_actors = Active_Actors()
+        #self._actor = Actor()
+
+    def clear_screen(self):
+        pass
+
+    def draw_word(self):
+        '''renders a word on the screen'''
+
+    def draw_words(self):
+        '''renders all the words in the list on the screen'''
+
+        #for word in words
+            #draw_word()
+        pass
+        
+        
