c814c79de4ccb2ac39748ed4a3bc1e0d7365694b
matthe68 <matthewbchrsitensen@comcast.net>
2021-06-05 02:28:37 -0600

    Made Score class and pytests. Updated comments in director and test_point

--- Speed/speed/game/director.py
+++ Speed/speed/game/director.py
@@ -23,8 +23,9 @@ class Director:
     Start the game loop to control the sequence of play. Something like the following from the snake game
         while self._keep_playing:
             self._get_inputs() - gets input of a single letter, adds them to a list of characters to display as buffer on the bottom of the screen (should we make a buffer class and set the position to the bottom of the screen? it might be easier that way)
-            self._do_updates() - did they get a word when the pressed enter or did a word reach the end of the screen? Yes- call funciton in active_actors to kill the word on the screen (if they got a word right also send it to score to get points). No- do nothing. clear buffer list if enter was pressed. move each word to its new position
-            self._do_outputs() - output new positions and current buffer to the screen
+            self._do_updates() - did they get a word when the pressed enter or did a word reach the end of the screen? Yes- call function in score to add points and input the word, call funciton in active_actors to kill the word on the screen. No- do nothing. clear buffer list if enter was pressed. move each word to its new position
+            self._do_outputs() - output Actors in new positions and add the current buffer and the current score to the screen
             sleep(constants.FRAME_LENGTH)
 
     """
+    def __init__(self, input_service, output_service):
--- Speed/tests/test_point.py
+++ Speed/tests/test_point.py
@@ -3,20 +3,24 @@ import pytest
 from speed.game.point import Point
 
 class Test_Point:
+    """Tests the class function get_x and its ability to return the x coordinate on the screen"""
     def test_get_x(self):
         point = Point(5,6)
         assert point.get_x() == 5
     
     def test_get_y(self):
+        """Tests the class function get_y and its ability to return the y coordinate on the screen"""
         point = Point(5,6)
         assert point.get_y() == 6
     
     def test_set_x(self):
+        """Tests the class function set_x and its ability to set the x coordinate on the screen"""
         point = Point(5,6)
         point.set_x(10)
         assert point.get_x() == 10
 
     def test_set_y(self):
+        """Tests the class function set_y and its ability to set the y coordintate on the screen"""
         point = Point(5,6)
         point.set_y(10)
         assert point.get_y() == 10
