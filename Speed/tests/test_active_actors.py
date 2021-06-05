7ecced3e569f2034353d0e85056be72145673f01
Morgan Mann <momomann4@gmail.com>
2021-06-05 17:43:38 -0600

    pytest for active_actors

--- Speed/tests/test_active_actors.py
+++ Speed/tests/test_active_actors.py
@@ -1,6 +1,6 @@
 """ class for testing active_actors class functions"""
 
-import pytest as py
+import pytest 
 from speed.game.active_actors import Active_Actors
 from speed.game.screen import screen
 
@@ -8,6 +8,7 @@ from speed.game.screen import screen
 class test_active_actors():
 
     def test_check_guess():
+        """Tests the class function check_guess and its ability to detrmine if the guess was correct or not"""
         active_actors = Active_Actors()
         active_actors._active_words = ["blue", "red", "green"]
         guess = "blue"
@@ -16,6 +17,7 @@ class test_active_actors():
 
 
     def test_actor_list():
+        """Tests the class function actor_list and its ability to create a list of the words provided"""
         active_actors = Active_Actors()
         active_actors._active_words = ["blue", "red", "green"]
         output = active_actors.actor_list 
@@ -23,11 +25,12 @@ class test_active_actors():
 
 
     def test_replace_word():
+        """Tests the class function replace_word and its ability to replace a word in the list with a new word"""
         active_actors = Active_Actors()
         active_actors.replace_word().new_word = "pink"
         active_actors._active_words = ["blue", "red", "green"]
         active_actors.replace_word(0)
         assert active_actors._active_words == ["pink", "red", "green"]
     
-    py.main(["-v", "--tb=no", "test_active_actors.py"]
+   
 
